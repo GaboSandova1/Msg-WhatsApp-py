a9cb0c77c7a64024caf9e82b0877518301b46bc7
GaboSandova1 <gsandoval21@ujap.edu.ve>
2025-05-02 22:05:30 -0400

    Actualizar comentario en msgWhatsApp.py para mayor claridad sobre el uso del código

--- msgWhatsApp.py
+++ msgWhatsApp.py
@@ -1,6 +1,5 @@
 
-# Este codigo funciona solo si no tienes una contrasena en tu WhatsApp Web como la tengo yo.
-# en ese caso comenta las lineas 4 y 6 y descomenta las demas lineas
+# Este codigo funciona solo si no tienes una extension que te permita poner una contrasena en tu WhatsApp Web
 import pywhatkit
 
 pywhatkit.sendwhatmsg("+584243811068", "No c q poner xd", 9, 22, 10, True, 2)
