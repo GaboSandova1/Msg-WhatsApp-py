ce3420694173cd4b41ead4c71079be3bb2f332e8
GaboSandova1 <gsandoval21@ujap.edu.ve>
2025-05-02 22:05:53 -0400

    Eliminar código innecesario y mejorar la claridad del archivo msgWhatsApp.py

--- msgWhatsApp.py
+++ msgWhatsApp.py
@@ -1,5 +1,5 @@
 
-# Este codigo funciona solo si no tienes una extension que te permita poner una contrasena en tu WhatsApp Web
+# Este codigo funciona solo si no tienes una extension o algo que te permita poner una contrasena en tu WhatsApp Web
 import pywhatkit
 
 pywhatkit.sendwhatmsg("+584243811068", "No c q poner xd", 9, 22, 10, True, 2)
